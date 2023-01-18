01a40d7f6d674ca3d1461349a2e6c6121f2920f3
Dat Nguyen <itsdatnguyen@gmail.com>
2023-01-18 14:48:22 -0500

    Added basic express server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3001
+
+const listings = require('./listings.json')
+
+app.use(cors());
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+app.get('/listings', (req, res) => {
+    // for now, I'm simulating a load time here, @TODO: remove this later.
+    setTimeout(() => {
+        res.json(listings);
+    }, 1000);
 })
 
 app.listen(port, () =>
