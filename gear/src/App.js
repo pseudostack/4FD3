66c1498e0c558d60e33a2a484bdca71580770b1e
Dat Nguyen <itsdatnguyen@gmail.com>
2023-01-17 20:05:40 -0500

    Initial Commit

---
+++ gear/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import { Button, Card, } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="danger" size="lg">
+          Whoa Dude
+        </Button>
+        <Card className='mb-4'>
+          <Card.Header>
+            Header
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>
+              My Title
+            </Card.Title>
+            Test
+          </Card.Body>
+        </Card>
+        <Card className='mb-4'>
+          Test2
+        </Card>
+      </header>
+    </div>
+  );
+}
+
+export default App;
